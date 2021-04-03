5bd82edf5361bd39a3a0950b272ba9fe96f7c9ba
Конырев Кирилл Андреевич <pl8416@C201N2.pl1.edu>
2021-04-03 11:56:19 +0500

    Save

---
+++ main.js
@@ -0,0 +1,23 @@
+let cube = {
+    id: "cube",
+    x: 0,
+    y: 0,
+    width: 56,
+    height: 56,
+    transition: .25,
+    step: 56,
+    color: "black",
+    
+}
+
+let map = {
+    width: 500,
+    height: 500,
+    x: 0,
+    y: 0,
+    color: "#f5f5f5",
+    border: "#f1f1f1"
+}
+
+renderCube(cube); 
+renderMap(map); 
